d309f8475d1a3015665ee636598e350d55547f25
Mohamed Tharwat <mthawrat@example.com>
2026-06-13 10:13:21 +0300

    Fix OOM issue, add browser deps, and bind 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -36,6 +36,6 @@ app.use('/api/auth', authRoutes);
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
